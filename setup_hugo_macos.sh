19c698871a8c1b37b0a83a3b5d6fdc1422ee176c
hayesall <alexander@batflyer.net>
2022-08-31 16:34:26 -0400

    ✨ Add macos setup script

--- setup_hugo_macos.sh
+++ setup_hugo_macos.sh
@@ -5,14 +5,14 @@
 
 # Goal: Download the `hugo` binary to the base of this repository.
 # Run:
-#           bash setup_hugo_linux.sh
+#           bash setup_hugo_macos.sh
 
 mkdir hugo_download
 
 (
     cd hugo_download
-    curl -L https://github.com/gohugoio/hugo/releases/download/v0.102.2/hugo_0.102.2_Linux-64bit.tar.gz > hugo_0.102.2_Linux-64bit.tar.gz
-    tar -zxvf hugo_0.102.2_Linux-64bit.tar.gz
+    curl -L https://github.com/gohugoio/hugo/releases/download/v0.102.2/hugo_0.102.2_macOS-universal.tar.gz > hugo_0.102.2_macOS-universal.tar.gz
+    tar -zxvf hugo_0.102.2_macOS-universal.tar.gz
     mv hugo ..
 )
 
